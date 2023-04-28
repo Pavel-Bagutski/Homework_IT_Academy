4310a370bb6cff2be29c1ad4ab83ff81fb184095
Pavel-Bagutski <pavelbagutski@gmail.com>
2023-04-28 19:46:35 +0300

    40-43 tasks

--- src/Homework/Task43/Main.java
+++ src/Homework/Task43/Main.java
@@ -6,24 +6,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "Это пример строки, содержащей содержащей содержащей несколько содержащей слов.";
-        String[] words = (text.split("[^а-яА-Я]+"));
+        Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> wordsReapet = new TreeMap<>();
+        String text = scanner.nextLine();
+        String[] words = (text.split("[^а-яА-Яa-zA-Z]+"));
+
+        Map<String, Integer> wordsRepeat = new TreeMap<>();
 
         for (String word : words) {
-            if (wordsReapet.containsKey(word)) {
-                int count = wordsReapet.get(word);
-                wordsReapet.put(word, count + 1);
+            if (wordsRepeat.containsKey(word)) {
+                int count = wordsRepeat.get(word);
+                wordsRepeat.put(word, count + 1);
             } else {
-                wordsReapet.put(word, 1);
+                wordsRepeat.put(word, 1);
             }
         }
 
-        for (Map.Entry<String, Integer> entry : wordsReapet.entrySet()) {
+        for (Map.Entry<String, Integer> entry : wordsRepeat.entrySet()) {
             String word = entry.getKey();
             int count = entry.getValue();
             System.out.println(word + ": " + count);
         }
+        scanner.close();
     }
 }
