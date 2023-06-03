fe8029ec8e0858ee7947932528318a6253110766
Pavel-Bagutski <pavelbagutski@gmail.com>
2023-06-03 15:57:53 +0300

    fixed task 38

--- src/Homework/Task38/Season.java
+++ src/Homework/Task38/Season.java
@@ -1,21 +1,29 @@
 package Homework.Task38;
 
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
 import java.util.Locale;
+import java.time.LocalDate;
 
 public enum Season {
-    WINTER ,
-    SPRING ,
-    SUMMER ,
-    AUTUMN ;
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN;
 
-    public static void printQuantityDays (Season season){
-        switch (season){
-            case WINTER -> System.out.println("90 days");
-            case SPRING -> System.out.println("92 days");
-            case SUMMER -> System.out.println("92 days");
-            case AUTUMN -> System.out.println("91 days");
+    public static void printQuantityDays(Season season) {
+        switch (season) {
+            case WINTER:
+                System.out.println("90 days");
+                break;
+            case SPRING:
+                System.out.println("92 days");
+                break;
+            case SUMMER:
+                System.out.println("92 days");
+                break;
+            case AUTUMN:
+                System.out.println("91 days");
+                break;
         }
     }
 }
